e097e0b86f6e9574d93de3bc25babde265f5103a
Juan Carlos <juan@thirstie.com>
2018-04-14 17:56:39 -0300

    First upload

---
+++ python_module.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"log"
+	"net"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+)
+
+// Define Functions here.
+type Echo int
+
+func (h *Echo) Echo(arg *string, reply *string) error {
+	log.Println("received: ", *arg)
+	*reply = *arg
+	return nil
+}
+
+func main() {
+	// Register Functions on the RPC here.
+	hello := new(Echo)
+	rpc.Register(hello)
+
+	listener, errors := net.Listen("tcp", "127.0.0.1:5090")
+	defer listener.Close()
+
+	if errors != nil {
+		log.Fatal(errors)
+	}
+
+	log.Print("Listening for RPC-JSON connections: ", listener.Addr())
+
+	for {
+		log.Print("Waiting for RPC-JSON connections...")
+		conection, errors := listener.Accept()
+
+		if errors != nil {
+			log.Printf("Accept error: %s", conection)
+			continue
+		}
+
+		log.Printf("Connection started: %v", conection.RemoteAddr())
+		go jsonrpc.ServeConn(conection)
+	}
+}
