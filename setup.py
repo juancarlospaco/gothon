c8111aed55883f774834e1e914ff5aa13cf0ccfb
Juan Carlos <juan@thirstie.com>
2018-04-15 20:49:08 -0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#
+# To generate DEB package from Python Package:
+# sudo pip3 install stdeb
+# python3 setup.py --verbose --command-packages=stdeb.command bdist_deb
+#
+#
+# To generate RPM package from Python Package:
+# sudo apt-get install rpm
+# python3 setup.py bdist_rpm --verbose --fix-python --binary-only
+#
+#
+# To generate EXE MS Windows from Python Package (from MS Windows only):
+# python3 setup.py bdist_wininst --verbose
+#
+#
+# To generate PKGBUILD ArchLinux from Python Package (from PyPI only):
+# sudo pip3 install git+https://github.com/bluepeppers/pip2arch.git
+# pip2arch.py PackageNameHere
+#
+#
+# To Upload to PyPI by executing:
+# sudo pip install --upgrade pip setuptools wheel virtualenv
+# python3 setup.py bdist_egg bdist_wheel --universal sdist --formats=zip upload --sign
+
+
+"""Generic Setup.py.
+ALL THE CONFIG LIVES IN SETUP.CFG,PLEASE EDIT THERE,KEEP IT SIMPLE AND CLEAN."""
+
+
+from setuptools import setup
+
+
+setup(py_modules=["gothon"])
